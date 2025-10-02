692506a1c60b1f4ed693291b5c8f1471818b94a0
fr1g <cheunghast@outlook.com>
2025-10-02 19:35:38 +0300

    reset template

---
+++ front.rn/metro.config.js
@@ -0,0 +1,7 @@
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require('nativewind/metro');
+
+const config = getDefaultConfig(__dirname)
+
+module.exports = withNativeWind(config, { input: './global.css' })
+// deprecated: because using nativeCN but using experimental nativewind v5
---
+++ front.rn/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{tsx, ts, jsx, js}", "./App.tsx", "./index.ts"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
