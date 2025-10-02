a2376db1e4ff903081a5f5f6e007fd4a491edb90
fr1g <cheunghast@outlook.com>
2025-10-03 02:26:05 +0300

    stabilize

--- front.rn/tailwind.config.js
+++ front.rn/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{tsx, ts, jsx, js}", "./App.tsx", "./index.ts"],
+  content: ["./src/**/*.{tsx, ts, jsx, js}", "./App.tsx", "./index.ts", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset"), require('./nativecn-preset')],
   theme: {
     extend: {},
   },
