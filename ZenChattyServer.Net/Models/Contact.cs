bb8066a1a2e88c0a273e949ac1e66f760c3b9608
fr1g <cheunghast@outlook.com>
2025-11-04 16:02:34 +0300

    Create Contact.cs

---
+++ ZenChattyServer.Net/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿namespace ZenChattyServer.Net.Models;
+
+public class Contact
+{
+    public User Host { get; set; }
+    public Chat Object { get; set; } // can be a group  or   private
+    public ushort LastUnreadCount { get; set; }
+    public DateTime LastUsed { get; set; }
+    public DateTime AddTime { get; set; } = DateTime.Now;
+    public bool IsPinned { get; set; } = false;
+    public bool IsBlocked { get; set; } = false;
+    public string? DisplayName { get; set; } = null;
+    
+}
