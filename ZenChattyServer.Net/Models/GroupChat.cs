3ea83e95252cc89d2ecdc3425fca9746db2289b3
fr1g <cheunghast@outlook.com>
2025-12-17 02:34:33 +0300

    -

--- ZenChattyServer.Net/Models/GroupChat.cs
+++ ZenChattyServer.Net/Models/GroupChat.cs
@@ -9,4 +9,8 @@ public class GroupChat(User initBy, List<GroupChatMember>? members = null) : Cha
     
     // 公告消息集合（软拷贝，存储被标记为公告的消息）
     public virtual ICollection<Message> AnnouncementMessages { get; set; } = new List<Message>();
+    /* todo
+     * 需要删除这个属性并改换为从Messages查询被标记为此群聊的公告的消息
+     * 获取群公告（不分页）则由Message类型中结合所属Chat的UniqueMark和其本身的Type进行查询
+     */
 }
